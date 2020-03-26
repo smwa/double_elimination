0e1c2f3bf110852a176a19a4d9019601c3666610
Michael Smith <michael.smith.ok@gmail.com>
2020-03-26 17:38:09 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="double_elimination-smwa",
+    version="1.0.0",
+    author="Michael Smith",
+    author_email="michael.smith.ok@gmail.com",
+    description="A double elimination tournament match handler.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/smwa/double_elimination",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
